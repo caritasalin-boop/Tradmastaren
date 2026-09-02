551337c0b7f7eaafe146a59ac040b4907d60795d
caritasalin-boop <carita.salin@gmail.com>
2026-09-02 20:03:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,12 @@
 const trees = [
 
-{
-name:"Björk",
-image:"images/bjork.jpg"
-},
-
-{
-name:"Lind",
-image:"images/lind.jpg"
-},
-
-{
-name:"Rönn",
-image:"images/rönn.jpg"
-},
-
-{
-name:"Lönn",
-image:"images/lönn.jpg"
-},
-
-{
-name:"Alm",
-image:"images/alm.jpg"
-},
-
-{
-name:"Ek",
-image:"images/ek.jpg"
-},
-
-{
-name:"Gran",
-image:"images/gran.jpg"
-},
-
-{
-name:"Tall",
-image:"images/tall.jpg"
-}
+{name:"Björk", image:"images/bjork.jpg"},
+{name:"Lind", image:"images/lind.jpg"},
+{name:"Rönn", image:"images/rönn.jpg"},
+{name:"Lönn", image:"images/lönn.jpg"},
+{name:"Alm", image:"images/alm.jpg"},
+{name:"Ek", image:"images/ek.jpg"},
+{name:"Gran", image:"images/gran.jpg"},
+{name:"Tall", image:"images/tall.jpg"}
 
 ];
-``
